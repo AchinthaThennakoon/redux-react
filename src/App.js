edc477f11afc7c9151c0182bb20d0e08e0a9ed9f
achintha <achinthathennakoon97@gmail.com>
2023-01-25 14:57:55 +0530

    add dispstch to call actions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./action";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const isLogged = useSelector((state) => state.isLogged);
 
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>counter : {counter}</h1>
-      <button>increment</button>
-      <button>decrement</button>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
       <hr />
       {isLogged ? "You are logged in" : "please login"}
     </div>
